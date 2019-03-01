67467036239fcfb060940373242ad2ba2f817ede
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2019-03-01 16:36:48 +0100

    Cleaned out old test

--- src/test/java/querydsl/SubstringTest.java
+++ src/test/java/querydsl/SubstringTest.java
@@ -1,11 +1,6 @@
 package querydsl;
 
-import com.querydsl.core.types.Constant;
-import com.querydsl.core.types.ConstantImpl;
 import com.querydsl.core.types.ExpressionUtils;
-import com.querydsl.core.types.Visitor;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.SQLTemplates;
 import com.querydsl.sql.spatial.PostGISTemplates;
@@ -15,7 +10,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import javax.annotation.Nullable;
 import javax.inject.Provider;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,36 +31,6 @@ public class SubstringTest {
     private Properties properties;
     private Connection connection;
     private Provider<Connection> connectionProvider;
-
-    public static class ConstantNumberExpression<N extends Number & Comparable<?>> extends NumberExpression<N> {
-
-        private static final long serialVersionUID = 1L;
-
-        public ConstantNumberExpression(final N constant) {
-            super(ConstantImpl.create(constant));
-        }
-
-        @Override
-        @Nullable
-        public <R, C> R accept(final Visitor<R, C> v, @Nullable final C context) {
-            return v.visit((Constant<N>) mixin, context);
-        }
-
-    }
-
-    public static class ConstantBooleanExpression extends BooleanExpression {
-
-        public ConstantBooleanExpression(final Boolean constant) {
-
-            super(ConstantImpl.create(constant));
-        }
-
-        @Override
-        @Nullable
-        public <R, C> R accept(Visitor<R, C> v, C context) {
-            return v.visit((Constant<Boolean>) mixin, context);
-        }
-    }
 
     public SQLQueryFactory createQueryFactory() {
         SQLTemplates templates = PostGISTemplates.builder().quote().build();
